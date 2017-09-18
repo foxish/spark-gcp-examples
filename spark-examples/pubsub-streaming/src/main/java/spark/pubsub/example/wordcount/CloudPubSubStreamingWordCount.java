e5e2331cc16d92537424298fd3383a83fabc2295
Yinan Li <liyinan926@gmail.com>
2017-09-17 17:37:52 -0700

    Added a Spark Streaming example that reads from Cloud PubSub

---
+++ spark-examples/pubsub-streaming/src/main/java/spark/pubsub/example/wordcount/CloudPubSubStreamingWordCount.java
@@ -0,0 +1,81 @@
+package spark.pubsub.example.wordcount;
+
+import com.google.cloud.hadoop.util.EntriesCredentialConfiguration;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.storage.StorageLevel;
+import org.apache.spark.streaming.Milliseconds;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.pubsub.PubsubUtils;
+import org.apache.spark.streaming.pubsub.SparkGCPCredentials;
+import org.apache.spark.streaming.pubsub.SparkPubsubMessage;
+import scala.Tuple2;
+
+/**
+ * A simple Spark Streaming example that gets words from a Cloud PubSub topic, computes the
+ * word counts, and writes the result of each batch to a file on GCS.
+ */
+public class CloudPubSubStreamingWordCount {
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length != 3) {
+            System.err.println("Usage: CloudPubSubStreamingWordCount <GCP project ID> " +
+                    "<Cloud PubSub topic name> <GCS output path>");
+            System.exit(1);
+        }
+
+        Preconditions.checkArgument(
+                !Strings.isNullOrEmpty(args[0]), "GCP project ID must not be null or empty");
+        Preconditions.checkArgument(
+                !Strings.isNullOrEmpty(args[1]), "Cloud PubSub topic name must not be empty");
+
+        JavaStreamingContext jsc = new JavaStreamingContext(
+                JavaSparkContext.fromSparkContext(SparkContext.getOrCreate()),
+                Milliseconds.apply(2000) // Batch duration
+        );
+
+        Configuration hadoopConf = jsc.sparkContext().hadoopConfiguration();
+        // Use service account for authentication. The service account key file is located at the path
+        // specified by the configuration property google.cloud.auth.service.account.json.keyfile.
+        hadoopConf.set(EntriesCredentialConfiguration.BASE_KEY_PREFIX +
+                        EntriesCredentialConfiguration.ENABLE_SERVICE_ACCOUNTS_SUFFIX,
+                "true");
+
+        // This will create a subscription to the given topic.
+        JavaReceiverInputDStream<SparkPubsubMessage> pubSubStream  = PubsubUtils.createStream(
+                jsc,
+                args[0], // GCP project ID
+                args[1], // Cloud PubSub topic name
+                "", // Cloud PubSub subscription (empty so one gets created)
+                new SparkGCPCredentials.Builder().build(),
+                StorageLevel.MEMORY_AND_DISK_SER_2());
+
+        JavaPairDStream<String, Long> wordCounts = pubSubStream
+                .mapToPair(word -> new Tuple2<>(new String(word.getData()), 1L))
+                .reduceByKey((count1, count2) -> count1 + count2);
+        wordCounts
+                .mapToPair(tuple -> new Tuple2<>(new Text(tuple._1), new LongWritable(tuple._2)))
+                .saveAsNewAPIHadoopFiles(
+                        String.format("%s/word-count", args[2] /* GCS output path */),
+                        "txt",
+                        Text.class,
+                        LongWritable.class,
+                        TextOutputFormat.class,
+                        hadoopConf);
+
+        try {
+            jsc.start();
+            jsc.awaitTermination();
+        } finally {
+            jsc.stop(true, true);
+        }
+    }
+}
